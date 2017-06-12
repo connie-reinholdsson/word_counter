9babbefeff40ac66d5d84bf7fed3c888ce71ef8d
connie-reinholdsson <connie.reinholdsson@gmail.com>
2017-06-12 13:13:35 +0100

    Fixed errors and typos

--- src/wordCounter.js
+++ src/wordCounter.js
@@ -4,7 +4,7 @@ function WordCounter(text){
 }
 
 var fs = require('fs');
-fs.readFile(book.txt, 'utf8', function(err, data) {
+fs.readFile('book.txt', 'utf8', function(err, data) {
   if(err) throw err;
 
 var wordCounter = new WordCounter(data);
